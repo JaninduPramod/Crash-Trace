5296d0a78036f47544f1187c8482595318328153
Dhananga <dhanangabuddhi@gmail.com>
2025-07-20 05:02:02 +0530

    report model changes

--- app/src/main/java/com/crashtrace/mobile/Server/src/models/Report.js
+++ app/src/main/java/com/crashtrace/mobile/Server/src/models/Report.js
@@ -1,19 +1,28 @@
 import mongoose from "mongoose";
+import AutoIncrementFactory from "mongoose-sequence";
+
+const AutoIncrement = AutoIncrementFactory(mongoose);
+
+
 
 const reportSchema = new mongoose.Schema(
   {
     vehicleNo: { type: String, required: true },
+    cardID: { type: Number, unique: true },
     imageUrl: { type: String ,required: false},
-    description: { type: String, required: true },
-    location: { type: [String], required: true }, 
+    description: { type: String, required: false },
+    location: { type: [String], required: false }, 
     address: { type: String },
     reporterId: { type:String, required: true },
-    date: { type: String, required: true },
-    isApproved: { type: Boolean, default: false },
+    date: { type: String, required: false },
+    status: { type: String, default: "pending" },
     trustRate: { type: Number, default: 0 }
   },
   { timestamps: { createdAt: true, updatedAt: false } }
 );
+
+reportSchema.plugin(AutoIncrement, { inc_field: "cardID" });
+
 
 const Report = mongoose.model("Report", reportSchema);
 export default Report;
