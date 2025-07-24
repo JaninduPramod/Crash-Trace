e4cab0b17d9766ee0f67200e5bd0ccb6bac8a3a4
Hansi tharuka <ehansitharuka2001@gmail.com>
2025-07-25 01:44:44 +0530

    Changed gradle file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,12 +6,12 @@ plugins {
 
 android {
     namespace = "com.crashtrace.mobile"
-    compileSdk = 35
+    compileSdk = 33
 
     defaultConfig {
         applicationId = "com.crashtrace.mobile"
-        minSdk = 34
-        targetSdk = 35
+        minSdk = 26
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
